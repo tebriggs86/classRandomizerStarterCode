367bc623067a7f2c570f86b2877157bc9da775e7
Trey Briggs <tebriggs86@gmail.com>
2017-02-27 01:45:44 +0000

    Randomizing teachers and students on click

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
+    var students = [
+        "Richard",
+        "Adrian",
+        "Gerardo",
+        "Tim",
+        "Jorge",
+        "Garrett",
+        "Spencer",
+        "Evila",
+        "Kaci",
+        "Isaac",
+        "Abby",
+        "Alexis",
+        "Thomas",
+        "Basil"];
+    var teachers = [
+        "Matt",
+        "Samuel",
+        "Sam",
+        "Trey"];
+    
+    $("#teacherButton").click(function () {
+        var randomTeacher = teachers[Math.floor(Math.random() * teachers.length)];
+        $("#teacherDisplay").html(randomTeacher + " is your teacher.");
+    });
 
-
+    $("#studentButton").click(function() {
+        var randomStudent = students[Math.floor(Math.random() * students.length)];
+        $("#studentDisplay").html(randomStudent + " is your student.");
+    });
 
 });
